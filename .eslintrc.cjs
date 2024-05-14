cba38bdfbfc7781d08e942262a970b05c53aac8c
erik-krogh <erik-krogh@github.com>
2024-05-14 22:17:06 +0200

    revert debug code in example app, and update it a bit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json", "./test/tsconfig.json"],
+    project: ["./tsconfig.json", "./exampleApp/tsconfig.json"],
     ecmaVersion: "latest",
     sourceType: "module",
   },
